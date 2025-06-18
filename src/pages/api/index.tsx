5f3bcc354e5256dfeb712d1a8bdbddbfdaf4d24f
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-06-18 06:53:35 -0400

    adicionando cfg's á api

--- src/pages/api/index.tsx
+++ src/pages/api/index.tsx
@@ -386,6 +386,7 @@ export const distrito: Local[] = [
 
 export const torres: TorreType[] = [
   {
+    //centro
     id: '1',
     nome: 'Centro',
     location: 'cidade',
@@ -501,6 +502,7 @@ export const torres: TorreType[] = [
     ]
   },
   {
+    //planalto
     id: '2',
     nome: 'Planalto',
     location: 'cidade',
@@ -529,6 +531,7 @@ export const torres: TorreType[] = [
     ]
   },
   {
+    //galeria
     id: '3',
     nome: 'Galeria',
     location: 'cidade',
@@ -552,45 +555,220 @@ export const torres: TorreType[] = [
     ]
   },
   {
+    //cap vigario
     id: '4',
     nome: 'Cap. Vigário',
     location: 'cidade',
-    equipamentos: [{ titulo: 'Roteador B', link: '/backups/roteador-b.zip' }]
+    equipamentos: [
+      {
+        titulo: 'Tec Net - PTP House 1 (10.254.92.2)',
+        link: '/backups/Cap_Vigario/tec_net_-_ptp_house_1_(10.254.92.2).cfg'
+      },
+      {
+        titulo: 'Tec Net - PTP House 2 (10.254.92.3)',
+        link: '/backups/Cap_Vigario/tec_net_-_ptp_house_2_(10.254.92.3).cfg'
+      },
+      {
+        titulo: 'TEC NET - Setor House 2 (10.92.2.2)',
+        link: '/backups/Cap_Vigario/tec_net_-_setor_house_2_(10.92.2.2).cfg'
+      },
+      {
+        titulo: 'TEC NET - Setor House 3 (10.92.3.2)',
+        link: '/backups/Cap_Vigario/tec_net_-_setor_house_3_(10.92.3.2).cfg'
+      }
+    ]
   },
   {
+    //polo industrial
     id: '5',
     nome: 'Polo Industrial',
     location: 'cidade',
     equipamentos: [{ titulo: 'Roteador B', link: '/backups/roteador-b.zip' }]
   },
   {
+    //adonai
     id: '6',
     nome: 'Adonai',
     location: 'cidade',
-    equipamentos: [{ titulo: 'Roteador B', link: '/backups/roteador-b.zip' }]
+    equipamentos: [
+      {
+        titulo: 'PTP - Adonai 1 (10.254.21.2)',
+        link: '/backup_torres/centro/ptp_-_adonai_1_(10.254.21.2).cfg'
+      },
+      {
+        titulo: 'PTP - Adonai 2 (10.254.21.3)',
+        link: '/backup_torres/centro/ptp_-_adonai_2_(10.254.21.3).cfg'
+      },
+      {
+        titulo: 'Setorial Adonai 4 (10.21.4.2)',
+        link: '/backup_torres/centro/setorial_adonai_4_(10.21.4.2).cfg'
+      },
+      {
+        titulo: 'TEC NET - Setorial Adonai 1 (10.21.1.2)',
+        link: '/backup_torres/centro/tec_net_-_setorial_adonai_1_(10.21.1.2).cfg'
+      },
+      {
+        titulo: 'TEC NET - Setorial Adonai 2 (10.21.2.2)',
+        link: '/backup_torres/centro/tec_net_-_setorial_adonai_2_(10.21.2.2).cfg'
+      },
+      {
+        titulo: 'TEC NET - Setorial Adonai 3 (10.21.3.2)',
+        link: '/backup_torres/centro/tec_net_-_setorial_adonai_3_(10.21.3.2).cfg'
+      },
+      {
+        titulo: 'TEC NET - Setorial Adonai 5 (10.21.5.2)',
+        link: '/backup_torres/centro/tec_net_-_setorial_adonai_5_(10.21.5.2).cfg'
+      },
+      {
+        titulo: 'TEC NET - Setorial Adonai 6 (10.21.6.2)',
+        link: '/backup_torres/centro/tec_net_-_setorial_adonai_6_(10.21.6.2).cfg'
+      }
+    ]
   },
   {
+    //graos de ouro
     id: '7',
     nome: 'Graos de Ouro',
     location: 'cidade',
     equipamentos: [{ titulo: 'Roteador B', link: '/backups/roteador-b.zip' }]
   },
   {
+    //campo alto
     id: '8',
     nome: 'Campo Alto',
     location: 'cidade',
-    equipamentos: [{ titulo: 'Roteador B', link: '/backups/roteador-b.zip' }]
+    equipamentos: [
+      {
+        titulo: 'PTP Campo Alto 2 (10.254.2.3)',
+        link: '/backup_torres/Campo_Alto/ptp_campo_alto_2_(10.254.2.3).cfg'
+      },
+      {
+        titulo: 'TEC-NET-CAMPO-ALTO (10.2.1.2)',
+        link: '/backup_torres/Campo_Alto/tec-net-campo-alto_(10.2.1.2).cfg'
+      },
+      {
+        titulo: 'TEC-NET-CAMPO-ALTO-2 (10.2.2.2)',
+        link: '/backup_torres/Campo_Alto/tec-net-campo-alto-2_(10.2.2.2).cfg'
+      },
+      {
+        titulo: 'TEC-NET-CAMPO-ALTO-3 (10.2.3.2)',
+        link: '/backup_torres/Campo_Alto/tec-net-campo-alto-3_(10.2.3.2).cfg'
+      }
+    ]
   },
   {
+    //aprazivel
     id: '9',
     nome: 'Aprazível',
     location: 'cidade',
-    equipamentos: [{ titulo: 'Roteador B', link: '/backups/roteador-b.zip' }]
+    equipamentos: [
+      {
+        titulo: 'APRAZIVEL 1 - PrismStation 5AC (10.5.25.2)',
+        link: '/backup_torres/aprazivel/aprazivel_1_-_prismstation_5ac_(10.5.25.2).cfg'
+      },
+      {
+        titulo: 'APRAZIVEL 2 - PrismStation 5AC (10.5.26.2)',
+        link: '/backup_torres/aprazivel/aprazivel_2_-_prismstation_5ac_(10.5.26.2).cfg'
+      },
+      {
+        titulo: 'APRAZIVEL 3 - PrismStation 5AC (10.5.27.2)',
+        link: '/backup_torres/aprazivel/aprazivel_3_-_prismstation_5ac_(10.5.27.2).cfg'
+      },
+      {
+        titulo: 'PTP - Posto San Fernando 1 (10.254.34.2)',
+        link: '/backup_torres/aprazivel/ptp_-_posto_san_fernando_2_(10.254.34.2).cfg'
+      },
+      {
+        titulo: 'PTP - Posto San Fernando 2 (10.254.34.3)',
+        link: '/backup_torres/aprazivel/ptp_-_posto_san_fernando_2_(10.254.34.3).cfg'
+      },
+      {
+        titulo: 'Setoria Aprazivel 1 (10.5.10.2)',
+        link: '/backup_torres/aprazivel/setoria_aprazivel_1_(10.5.10.2).cfg'
+      },
+      {
+        titulo: 'Tec Net - Aprazivel - 2 (10.5.20.2)',
+        link: '/backup_torres/aprazivel/tec_net_-_aprazivel_-_2_(10.5.20.2).cfg'
+      },
+      {
+        titulo: 'TEC NET - Omni Posto (10.254.34.4)',
+        link: '/backup_torres/aprazivel/tec_net_-_omni_posto_(10.254.34.4).cfg'
+      },
+      {
+        titulo: 'TEC NET APRAZIVEL 1 (10.5.1.2)',
+        link: '/backup_torres/aprazivel/tec_net_aprazivel_1_(10.5.1.2).cfg'
+      },
+      {
+        titulo: 'TEC NET APRAZIVEL 2 (10.5.2.2)',
+        link: '/backup_torres/aprazivel/tec_net_aprazivel_2_(10.5.2.2).cfg'
+      },
+      {
+        titulo: 'TEC NET APRAZIVEL 3 (10.5.3.2)',
+        link: '/backup_torres/aprazivel/tec_net_aprazivel_3_(10.5.3.2).cfg'
+      },
+      {
+        titulo: 'TEC NET APRAZIVEL 4 (10.5.4.2)',
+        link: '/backup_torres/aprazivel/tec_net_aprazivel_4_(10.5.4.2).cfg'
+      },
+      {
+        titulo: 'TEC NET APRAZIVEL 5 (10.5.5.2)',
+        link: '/backup_torres/aprazivel/tec_net_aprazivel_5_(10.5.5.2).cfg'
+      }
+    ]
   },
   {
+    //horta
     id: '10',
     nome: 'Horta',
     location: 'cidade',
     equipamentos: [{ titulo: 'Roteador B', link: '/backups/roteador-b.zip' }]
+  },
+  {
+    //adm
+    id: '11',
+    nome: 'ADM',
+    location: 'rural',
+    equipamentos: [
+      {
+        titulo: 'AP- ADM 2 (10.254.24.3)',
+        link: '/backup_torres/ADM/ap-_adm_2_(10.254.24.3).cfg'
+      },
+      {
+        titulo: 'PTP TEC NET - ADM 1 (10.254.24.2)',
+        link: '/backup_torres/ADM/ptp_tec_net_-_adm_1_(10.254.24.2).cfg'
+      }
+    ]
+  },
+  {
+    //agua doce
+    id: '12',
+    nome: 'Agua Doce',
+    location: 'rural',
+    equipamentos: [
+      {
+        titulo: 'TEC NET - PTP Agua Doce 2 (10.70.3.4)',
+        link: '/backup_torres/Agua_Doce/tec_net_-_ptp_agua_doce_2_(10.70.3.4).cfg'
+      },
+      {
+        titulo: 'TEC NET - Agua Doce Omni (10.70.3.5)',
+        link: '/backup_torres/Agua_Doce/tec_net_-_agua_doce_omni_(10.70.3.5).cfg'
+      }
+    ]
+  },
+  {
+    //carreteira lins
+    id: '13',
+    nome: 'Carreteira Lins',
+    location: 'rural',
+    equipamentos: [
+      {
+        titulo: 'PTP - FAZ CARRETEIRA (Lins) (10.70.50.4)',
+        link: '/backups/Carreteira_(lins)/tec_net_carreteira_lins_1_(10.70.50.5).cfg'
+      },
+      {
+        titulo: 'TEC NET CARRETEIRA LINS 1 (10.70.50.5)',
+        link: '/backups/Carreteira_(lins)/tec_net_carreteira_lins_1_(10.70.50.5).cfg'
+      }
+    ]
   }
 ]
